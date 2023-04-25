66ba5a8aee6523385f07d7e9643cdbf29d8905f8
Kadu <kadu.silva2014@gmail.com>
2023-04-25 18:08:56 -0300

    🧹 chore(store/users): update store with new actions and refactor
    Refactored the users store to include new actions and improved the typing. The new actions added
    are: getClients, getMechanics, filterClientsByName. The getAllUsers action was also updated to
    return an array of TClient and TMechanic. Additionally, improved the code formatting and variable
    names for better readability.

--- src/store/users/index.ts
+++ src/store/users/index.ts
@@ -6,33 +6,53 @@ import { TClient, TMechanic } from '../../../@types';
 type ActionsProps = {
   addUser: (user: TClient | TMechanic) => void;
   getUser: (id: string) => TClient | TMechanic | undefined;
+  getClients: () => TClient[];
+  getMechanics: () => TMechanic[];
   getAllUsers: () => (TClient | TMechanic)[];
+  filterClientsByName: (name: string) => TClient[];
 };
 
 export type UsersStoreProps = {
   state: (TClient | TMechanic)[];
-  actions: ActionsProps;
-};
+} & ActionsProps;
 
-export const useUsersStore = create<UsersStoreProps>()(
+export const useUsersStore = create(
   devtools(
-    persist(
+    persist<UsersStoreProps>(
       (set, get) => ({
         state: [],
-        actions: {
-          addUser(user) {
-            set((state) => ({ state: [...state.state, user] }));
-          },
-          getUser(id) {
-            return get().state.find((user) => user.id === id);
-          },
-          getAllUsers() {
-            return get().state;
-          },
+        addUser: (user) => {
+          set((state) => ({
+            state: [...state.state, user],
+          }));
+        },
+        getUser: (id) => {
+          const users = get().state;
+          return users.find((user) => user.id === id);
+        },
+        getAllUsers: () => {
+          const users = get().state;
+          return users;
+        },
+        getClients: () => {
+          const users = get().state;
+          return users.filter((user) => user.role === 'client') as TClient[];
+        },
+        getMechanics: () => {
+          const users = get().state;
+          return users.filter(
+            (user) => user.role === 'mechanic',
+          ) as TMechanic[];
+        },
+        filterClientsByName: (name) => {
+          const clients = get().getClients();
+          return clients.filter((client) =>
+            client.name.toLowerCase().includes(name.toLowerCase()),
+          );
         },
       }),
       {
-        name: 'users-store',
+        name: 'usersStore',
       },
     ),
   ),
